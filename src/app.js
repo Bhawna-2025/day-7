8b0d709a5ad9ff1bbcd5cc3033c934cf70a78644
CodeYourLifeWith-BHAWII <pbhawna2023@gmail.com>
2026-02-20 12:05:39 +0530

    day-7|all done

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const noteModel=require("./models/notes.model.js")
 const app=express()
 
 app.use(express.json())
-
+//  post request to create a note in database
 app.post("/notes",async(req,res)=>{
 
     const {title,description}=req.body
@@ -16,5 +16,12 @@ app.post("/notes",async(req,res)=>{
         notes
     })
 })
-
+//get request to fetch all the data from the database 
+app.get("/notes",async(req,res)=>{
+    const notes= await noteModel.find()
+    res.status(200).json({
+        message:"notes fetched successfully",
+        notes
+    })
+})
 module.exports=app
